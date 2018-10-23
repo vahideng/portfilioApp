b5a63ec602ef52dd675c1df9534720b66a6086f6
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-23 11:57:41 +0800

    deploying app on github

---
+++ build/precache-manifest.875010889fc8b827b7bf55bdbde95b30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "252a6ad58a7bf668e00728e6cb075343",
+    "url": "/static/media/Screenshot 2018-10-21 at 12.57.43 PM.252a6ad5.png"
+  },
+  {
+    "revision": "5410e16c1d66bfdf7e8110da95d574ac",
+    "url": "/static/media/Screenshot 2018-10-17 at 4.30.03 PM.5410e16c.png"
+  },
+  {
+    "revision": "426f7ab4027ed8e4f178dab7b91a9992",
+    "url": "/static/media/Binary-Code-Art-1920x1080-Hi-Res.426f7ab4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad44cdea4238bbec2e5b",
+    "url": "/static/js/main.ad44cdea.chunk.js"
+  },
+  {
+    "revision": "5d92fc7bf677c9b0c2dc",
+    "url": "/static/js/1.5d92fc7b.chunk.js"
+  },
+  {
+    "revision": "ad44cdea4238bbec2e5b",
+    "url": "/static/css/main.b3f7e63b.chunk.css"
+  },
+  {
+    "revision": "eeee9ae5895b7a4f4a2301e8517998c4",
+    "url": "/index.html"
+  }
+];
